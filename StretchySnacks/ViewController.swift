0533da581f1f2c430c458834c10a9e7d936e4fb5
callosaurus <callumdavies421@gmail.com>
2017-03-17 14:03:56 -0700

    Set up custom nav bar and plusButton press behaviour

--- StretchySnacks/ViewController.swift
+++ StretchySnacks/ViewController.swift
@@ -10,7 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var isExpanded: Bool = false
+    var initialPlusPosition: CGAffineTransform?
+
     
+    @IBOutlet weak var navBarView: UIView!
+    
+    @IBOutlet weak var navBarHeightConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var plusButton: UIButton! {
+        didSet {
+            self.initialPlusPosition = plusButton.transform
+        }
+    }
+    @IBAction func plusIconPressed(_ sender: UIButton) {
+        isExpanded = !isExpanded
+        
+        UIView.animate(withDuration: 0.5, delay: 0.0, usingSpringWithDamping: 0.3, initialSpringVelocity: 1.0, options: .curveEaseIn, animations: ({
+            if self.isExpanded
+            {
+                self.plusButton.transform = CGAffineTransform(rotationAngle: CGFloat(M_PI_4))
+                self.navBarHeightConstraint.constant = 200
+            }
+            else
+            {
+                self.plusButton.transform = self.initialPlusPosition!
+                self.navBarHeightConstraint.constant = 66
+            }
+            
+        }), completion: nil)
+    }
 }
 
 
